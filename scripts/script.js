cbcc35bea2b47c1f42454ad71c7976381773be91
Rimon Mostafiz <rimonmostafiz@gmail.com>
2025-04-24 04:35:35 -0500

    Click support: because users love pressing things.

--- scripts/script.js
+++ scripts/script.js
@@ -10,4 +10,28 @@ function setTodayDate() {
     calenderDiv.lastElementChild.innerText = today.toLocaleDateString('en-US', dateFormat).replace(',', '');
 }
 
+function clickCompletedButton() {
+    const completedButtons = document.getElementsByClassName("completed-btn");
+    for (const item of completedButtons) {
+        console.log(item);
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            item.disabled = true;
+            createMessageElement(item);
+        })
+    }
+}
+
+function createMessageElement(item) {
+    const clickTime = new Date().toLocaleTimeString('en-US', timeFormat);
+    const taskTitle = item.parentElement.parentElement.previousElementSibling.previousElementSibling.firstElementChild.innerText;
+    const taskCompleteMessage = 'You have Completed The Task ' + taskTitle + ' at ' + clickTime;
+    const taskCompleteMessageDiv = document.getElementById("task-complete-message");
+    const taskCompleteMessageElement = document.createElement('p');
+    taskCompleteMessageElement.textContent = taskCompleteMessage;
+    taskCompleteMessageElement.classList.add('bg-rootBackground', 'p-2', 'text-left', 'rounded-md', 'mt-4', 'text-sm');
+    taskCompleteMessageDiv.appendChild(taskCompleteMessageElement);
+}
+
 setTodayDate();
+clickCompletedButton();
